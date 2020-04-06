d1b88d5f18c0b9c56b0609afb6767dab39e9ab23
rud9321 <rud9321@gmail.com>
2020-04-07 00:55:50 +0530

    initial commit...

---
+++ multiple-services/src/microservices-core-api-student/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace microservices_core_api_student.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                rollno = 21,
+                studentid = "rud",
+                name = "Rudra",
+                password = "123$"
+
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ multiple-services/src/microservices-core-api-user/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace microservices_core_api_user.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {   
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                username = "rud9321",
+                userid = "rud",
+                name= "Rudra",
+                password= "123$"
+
+            };
+            return Ok(result);
+        }
+    }
+}
